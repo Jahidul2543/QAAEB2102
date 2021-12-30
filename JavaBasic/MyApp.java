9ed1ba100dabe049c0d6e108f4074b1d83e147c6
jahidul2543 <jahidul2543@gmail.com>
2021-12-30 06:06:32 +0600

    Added gitignore file

---
+++ JavaBasic/MyApp.java
@@ -0,0 +1,10 @@
+public class MyApp{
+
+public static void main(String args[]){
+      System.out.println("This is my first app");
+      System.out.println(args[0]);
+      System.out.println(args[1]);
+
+   }
+}
+
