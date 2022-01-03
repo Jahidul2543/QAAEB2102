3a775126bfccc41950f8489ea1783d542c3c0722
jahidul2543 <jahidul2543@gmail.com>
2022-01-03 08:44:37 +0600

    Class_17 Java 103

---
+++ B2102JavaBasic/src/io/izaan/class103/Phone.java
@@ -0,0 +1,42 @@
+package io.izaan.class103;
+
+public class Phone {
+
+    // Phone jahidulsPhone = new Phone();
+    
+    public Phone(){
+        // No return type in constructor
+        System.out.println("Inside Default constructor");
+    }
+
+    public static void main(String[] args) {
+        Phone jahidulsPhone = new Phone();
+       boolean connectionStatus = jahidulsPhone.call(3474134475l); // true / false
+        System.out.println("Connection Status: " + connectionStatus);
+        jahidulsPhone.text(3474134475l);
+    }
+
+    public boolean call(long phoneNumber){
+        System.out.println("Calling....." + phoneNumber);
+        boolean isConnected = false;
+        boolean receivesByReceiver = false;
+        
+        if(receivesByReceiver){
+            isConnected = true;
+            System.out.println("Connected");
+            return isConnected;
+        }
+        else if (!receivesByReceiver){
+            isConnected = false;
+            System.out.println("Unable to connect");
+            return isConnected;
+        }
+            
+        return isConnected;
+    }
+
+    public void text(long phoneNumber){
+        System.out.println("Texting....." + phoneNumber);
+    }
+
+}
---
+++ B2102JavaBasic/src/io/izaan/class103/StringDemo.java
@@ -0,0 +1,12 @@
+package io.izaan.class103;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String name = "Johnjjjjjjjjjjjj";
+        System.out.println(name);
+        String myName = new String("John");
+        System.out.println(myName);
+        int lenghtOfName = name.length();
+        System.out.println("Lenght of Name: " + lenghtOfName);
+    }
+}
